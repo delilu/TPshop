cbad7efb06ce591e0b4037c26a0515bc8a38cdab
delilu <1144908933@qq.com>
2018-08-25 23:51:16 +0800

    添加发现显示隐藏密码的test_show_password测试方法并在allure报告中截图

--- page/login_page.py
+++ page/login_page.py
@@ -8,6 +8,8 @@ class LoginPage(BaseAction):
     username_btn = By.ID, "com.tpshop.malls:id/edit_phone_num"
     password_btn = By.ID, "com.tpshop.malls:id/edit_password"
     login_btn = By.ID, "com.tpshop.malls:id/btn_login"
+    # 显示密码的小眼睛按钮
+    view_pwd_btn = By.ID,"com.tpshop.malls:id/img_view_pwd"
 
     @allure.step(title="输入用户名")
     def input_username(self, text):
@@ -23,5 +25,10 @@ class LoginPage(BaseAction):
     def click_login(self):
         self.click(self.login_btn)
 
+    @allure.step(title="登录按钮是否可用")
     def login_btn_is_enabled(self):
         return self.is_location_enabled(self.login_btn)
+
+    @allure.step(title="点击显示密码按钮")
+    def click_view_pwd(self):
+        self.click(self.view_pwd_btn)
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,9 +1,28 @@
+import random
+import time
+
 import allure
 import pytest
+from allure.constants import AttachmentType
+from selenium.webdriver.common.by import By
 
 from base.base_analyze import analyze_with_file
 from base.base_driver import init_driver
 from page.page import Page
+
+
+def random_password():
+    password = ""
+    for i in range(6):
+        password += str(random.randint(0, 9))
+    return password
+
+
+def show_password_data():
+    temp_list = list()
+    for i in range(2):
+        temp_list.append(random_password())
+    return temp_list
 
 
 class TestLogin:
@@ -34,3 +53,18 @@ class TestLogin:
         self.page.login.input_username(username)
         self.page.login.input_password(pwd)
         assert not self.page.login.login_btn_is_enabled()
+
+    @allure.step(title="密码是否可显示隐藏")
+    @pytest.mark.parametrize("pwd", show_password_data())
+    def test_show_password(self, pwd):
+        pwd_location = (By.XPATH, "//*[@text='%s']" % pwd)
+        self.page.home.click_mine()
+        self.page.mine.click_login_sign_up()
+        self.page.login.input_password(pwd)
+        # 在点击显示密码按钮之前，是要找不到输入的密码
+        assert not self.page.login.is_location_exist(pwd_location)
+        self.page.login.click_view_pwd()
+        time.sleep(3)
+        # 显示密码时截图
+        allure.attach("显示密码:", self.driver.get_screenshot_as_png(), AttachmentType.PNG)
+        assert self.page.login.is_location_exist(pwd_location)
